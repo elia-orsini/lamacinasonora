9b29e217c9c38d98c77db11e8920389f6f6a675a
Your name here <elia.orsini@hotmail.com>
2022-02-04 21:17:44 +0000

    update home-video

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -48,9 +48,9 @@ function Menu() {
 
       {
       show ? 
-          <video className='absolute z-0 transition ease-in-out transform duration-2000 top-0 opacity-0 xl:opacity-100 left-1/2 -translate-x-full mx-auto min-h-screen max-h-screen' src={'bot.mp4'} autoplay="true" loop="true" playsinline="true" muted="true"/>
+          <video className='absolute z-0 transition ease-in-out transform duration-2000 top-0 opacity-0 xl:opacity-100 left-1/2 -translate-x-full mx-auto min-h-screen max-h-screen' src={'bot2.mp4'} autoplay="true" loop="true" playsinline="true" muted="true"/>
         : 
-          <video className='absolute z-0 transition ease-in-out transform duration-2000 top-0 left-1/2 -translate-x-1/2 mx-auto min-h-screen max-h-screen' src={'bot.mp4'} autoplay="true" loop="true" playsinline="true" muted="true"/>
+          <video className='absolute z-0 transition ease-in-out transform duration-2000 top-0 left-1/2 -translate-x-1/2 mx-auto min-h-screen max-h-screen' src={'bot2.mp4'} autoplay="true" loop="true" playsinline="true" muted="true"/>
       }
 
       {
