d8deeddba7ba617bd8c00eca5cf39c620736fffe
Your name here <elia.orsini@hotmail.com>
2022-02-08 23:11:21 +0000

    added socials section

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -5,6 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Menu() {
   const [show, setShow] = useState(false);
+  const [contacts, setContacts] = useState(true);
 
 
   return (
@@ -19,28 +20,45 @@ function Menu() {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div className="absolute z-10 tracking-widerleading-2 top-32 xl:top-10 right-7 max-w-full text-lg xl:text-3xl text-right font-semibold text-white">
+        <div className="absolute max-h-screen z-10 tracking-wider leading-2 top-32 md:top-10 xl:top-10 xl:right-7 right-3 xl:left-[52rem] md:left-[42rem] left-2 max-w-full text-base md:text-lg xl:text-xl text-right font-semibold text-white">
           <p>
             <a target="_blank" className="hover:underline underline-offset-2" href="https://www.facebook.com/InsomniaPageOfficial/photos/?ref=page_internal">photos</a> <br />
-            <a target="_blank" className="hover:underline underline-offset-2" href="https://fb.watch/8f-8HncXZD/">aftermovie</a> <br /><br />
-            <span className="font-light tracking-wide">
-              IN$0MN1A nasce come un modo per <br />esprimersi artisticamente attraverso eventi e <br />progetti musicali.
-              Il suono di IN$0MN1A è <br />basato sull’Hip-Hop, la Trap e la Bass Music <br />con una curatissima selezione musicale.<br />
-              La nostra comunicazione cerca di unire due scene,<br /> quella hip-hop (più colorata e
-              appariscente) e <br />quella elettronica (più minimale e misteriosa), <br />utilizzando grafiche scure
-              ma accattivanti <br />e rivisitando l’arte classica con la modernità <br />dell’estetica informatica.
+            <a target="_blank" className="hover:underline underline-offset-2" href="https://fb.watch/8f-8HncXZD/">aftermovie</a> <br />
+            {contacts ?
+            (<button onClick={() => setContacts(!contacts)} className="absolute hover:underline underline-offset-2 xl:right-1 right-2 font-semibold">contacts</button>)
+            :
+            (
+            <div className="absolute right-0 space-x-6 mt-1 mr-2 text-base">
+              <a href="mailto:info@insomniaevents.xyz"><i class="bi bi-envelope"></i></a>
+              <a target="_blank" href="https://facebook.com/InsomniaPageOfficial"><i class="bi bi-facebook"></i></a>
+              <a target="_blank" href="https://instagram.com/insomnia.01"><i class="bi bi-instagram"></i></a>
+            </div>
+            )
+            }
+            <br /><br />
+            <span className="w-[32rem] font-light tracking-wide overflow-scrollable">
+              IN$0MN1A nasce come un modo per esprimersi artisticamente attraverso eventi e progetti musicali.
+              Il suono di IN$0MN1A è basato sull’Hip-Hop, la Trap e la Bass Music con una curatissima selezione musicale.
+              La nostra comunicazione cerca di unire due scene, quella hip-hop (più colorata e
+              appariscente) e quella elettronica (più minimale e misteriosa), utilizzando grafiche scure
+              ma accattivanti e rivisitando l’arte classica con la modernità dell’estetica informatica.
             </span>
           </p> 
         </div>
-        <div className="absolute z-10 bottom-24 tracking-widest leading-6 right-48 max-w-full text-sm text-right font-thin text-white">
-          <p className="text-white text-left mt-32 mr-96">
+        <div className="absolute z-10 bottom-4 xl:block hidden tracking-widest leading-6 right-48 max-w-full text-sm text-right font-thin text-white">
+          <p className="text-white text-left mt-32 mr-[16rem]">
             return &#123; <br />
             &nbsp;&nbsp;&nbsp;&nbsp; const <span className="font-extrabold">sounds</span> = [ <br /> 
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:font-semibold" href="https://instagram.com/lucafontana.mp3">lucafontana</a><br /> 
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:font-semibold" href="https://instagram.com/alessio.dali">dali</a><br /> 
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:font-semibold" href="https://instagram.com/rbxy71">xy71</a><br /> 
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:font-semibold" href="https://instagram.com/alberto_comini">como</a><br /> 
-            &nbsp;&nbsp;&nbsp;&nbsp; ] <br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/lucafontana.mp3">lucafontana</a><br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/alessio.dali">dali</a><br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/rbxy71">xy71</a><br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/alberto_comini">como</a><br /> 
+            &nbsp;&nbsp;&nbsp;&nbsp; ], <br />
+            &nbsp;&nbsp;&nbsp;&nbsp; const <span className="font-extrabold">visuals</span> = [ <br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/elig.graphics">elig.graphics</a><br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/kristoffer.castro">kristoffer.castro</a><br /> 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a className="hover:underline" href="https://instagram.com/elig.ace">elig.ace</a><br /> 
+            &nbsp;&nbsp;&nbsp;&nbsp; ], <br />  
             &#125; 
           </p> 
         </div>
@@ -55,13 +73,13 @@ function Menu() {
 
       {
       show ? 
-        <button className="absolute bottom-16 xl:bottom-12 right-6 xl:right-14 bg-black rounded-full font-semibold text-white border px-2 h-12 xl:h-16 w-12 xl:w-16 p-0 border-white flex max-w-xl hover:bg-white hover:text-black hover:border-black transition-colors duration-500" onClick={() => setShow(!show)}>
+        <button className="z-10 absolute bottom-16 xl:bottom-12 right-6 xl:right-14 bg-black rounded-full font-semibold text-white border px-2 h-12 xl:h-16 w-12 xl:w-16 p-0 border-white flex max-w-xl hover:bg-white hover:text-black hover:border-black transition-colors duration-500" onClick={() => setShow(!show)}>
           <svg className="transition-transform ease-in-out rotate-180 duration-2000" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16" width="100%" height="100%">
             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
           </svg>
         </button>
         : 
-        <button className="absolute bottom-16 xl:bottom-12 right-6 xl:right-14 bg-black rounded-full text-white text-center border px-2 h-12 xl:h-16 w-12 xl:w-16 p-0 border-white flex hover:bg-white hover:text-black hover:border-black transition-colors duration-500" onClick={() => setShow(!show)}>
+        <button className="z-10 absolute bottom-16 xl:bottom-12 right-6 xl:right-14 bg-black rounded-full text-white text-center border px-2 h-12 xl:h-16 w-12 xl:w-16 p-0 border-white flex hover:bg-white hover:text-black hover:border-black transition-colors duration-500" onClick={() => setShow(!show)}>
           <svg className="transition-transform ease-in-out duration-2000" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16" width="100%" height="100%">
             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
           </svg>
--- src/Title.jsx
+++ src/Title.jsx
@@ -6,8 +6,8 @@ function Title() {
         <div className='overflow-hidden absolute left-2 xl:left-10 text-center z-10'>
             <img className="block mt-5 xl:mt-10 h-8 xl:h-11 xl:w-50" src="insomnia-logo-min.png" />
             <img className="xl:ml-1 ml-0.5 mt-1 block mt-1 h-7 xl:h-9 xl:w-52" src="symbols-bar.jpg" />
-            <img className="xl:ml-3 ml-1.5 mt-2 block mt-2 w-36 xl:w-48" src="bar.png" />
-            <p className='xl:mt-1 mt-2 xl:ml-0 ml-0.3 text-xs xl:text-base text-white tracking-wider xl:tracking-wider'>Music | Events | Culture</p>
+            <img className="xl:ml-[0.5rem] ml-1.5 mt-2 block mt-2 w-36 xl:w-[12.5rem]" src="bar.png" />
+            <p className='xl:mt-2 mt-2 xl:ml-[0.1rem] ml-0.3 text-xs xl:text-[0.94rem] text-white tracking-wider xl:tracking-widest'>Music | Events | Culture</p>
         </div>
     );
 }
