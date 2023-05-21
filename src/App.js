1145bb713e28b756145f21127c66e84f6092b588
Your name here <elia.orsini@hotmail.com>
2023-05-21 13:34:25 +0100

    fix: meta tags

--- src/App.js
+++ src/App.js
@@ -10,18 +10,6 @@ function App() {
         <title>La Macina Sonora</title>
         <meta charSet="utf-8" />
         <meta name="description" content="La Macina Sonora. Studio di registrazione audio" />
-
-        <meta property="og:title" content="La Macina Sonora" />
-        <meta property="og:description" content="La Macina Sonora: studio di registrazione, produzione, servizi di Mixing e Mastering, service audio e network artistico per realizzare musica, contenuti multimediali ed eventi." />
-        <meta property="og:image" content="/macina-logo.png" />
-        <meta name="og:site_name" content="La Macina Sonora" />
-        <meta property='twitter:title' content="La Macina Sonora" />
-        <meta property='twitter:image' content="/macina-logo.png"/>
-        <meta name="twitter:card" content="summary_large_image"/>   
-
-        <meta name="description" content="La Macina Sonora: studio di registrazione, produzione, servizi di Mixing e Mastering, service audio e network artistico per realizzare musica, contenuti multimediali ed eventi." />
-        <meta name="keywords" content="Brescia, music studio, recording, sala di registrazione, studio musicale, Montichiari, musica, Rino Bellandi" />
-        <meta name="author" content="Rino Bellandi" />
       </Helmet>
       <div className='absolute top-0 left-0 bg-white h-screen w-screen'>
           <Title />
