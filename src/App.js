0b15ad8b3dd5f37acb4db9cab6119bbe0168c020
Your name here <elia.orsini@hotmail.com>
2024-01-05 18:44:52 +0000

    refactor: remove react-helmet as dependency

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import Menu from './Menu.jsx';
 import Title from './Title.jsx';
 import './output.css';
-import Helmet from 'react-helmet';
 
 function App() {
   return (
     <>
-      <Helmet>
-        <title>La Macina Sonora</title>
-        <meta charSet="utf-8" />
-        <meta name="description" content="La Macina Sonora. Studio di registrazione audio" />
-      </Helmet>
       <div className='absolute top-0 left-0 bg-white h-screen w-screen'>
           <Title />
           <Menu />
